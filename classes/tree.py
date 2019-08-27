758229e7b3b8f4993844766215e65930e4220538
Leon ROUX <Nurrl@users.noreply.github.com>
2019-08-27 16:49:16 +0200

    Finished

--- classes/tree.py
+++ classes/tree.py
@@ -2,37 +2,61 @@ from classes.bintree import BinTree
 from classes.bitarray import BitArray
 
 class Tree:
-  def __init__(self, string):
-    self.weights = self._getweight(string)
+  def __init__(self, string, tree = None):
     self.bytes = '';
-    self.lookup = {}
 
-    # Generate binary tree from weights
-    while len(self.weights) > 1:
-      left = self.weights.pop(-1)
-      right = self.weights.pop(-1)
+    # Recover tree from compressed tree binary
+    if tree:
+      treebin = BitArray.from_bytes(tree)
+      self.tree = self.from_binary(treebin)[1]
+    else:
+      weights = self._getweight(string)
 
-      self.weights = [(left[0] + right[0], BinTree(None, left, right))] + self.weights
+      # Generate binary tree from weights
+      while len(weights) > 1:
+        left = weights.pop(-1)
+        right = weights.pop(-1)
+        if type(left) is not BinTree:
+          left = BinTree(left, None, None)
+        if type(right) is not BinTree:
+          right = BinTree(right, None, None)
 
-    self.tree = self.weights[0]
+        weights = [BinTree(None, left, right)] + weights
+
+      self.tree = BinTree(None, BinTree('\0', None, None), weights[0])
+
+  def from_binary(self, treebin):
+    if not treebin:
+      return (None, None)
+    tree = BinTree(None, None, None)
+    node = treebin.pop(0)
+
+    if node == 0:
+      (treebin, tree.left) = self.from_binary(treebin)
+      (treebin, tree.right) = self.from_binary(treebin)
+    if node == 1:
+      tree.key = [treebin.pop(0) for _ in range(8)][::-1]
+      tree.key = BitArray.to_bytes(tree.key)[0]
+
+    return (treebin, tree)
 
   def as_dict(self, tree = None, recurse = False, path = None):
     if not recurse:
       tree = self.tree;
-    tree = tree[1];
+      self.lookup = {}
     path = path or []
 
-    if tree.left:
-      if type(tree.left[1]) is BinTree:
+    if tree and tree.left:
+      if tree.left.key == None:
         self.as_dict(tree.left, True, path + [0])
       else:
-        self.lookup[tree.left[1]] = path + [0]
+        self.lookup[tree.left.key] = path + [0]
 
-    if tree.right:
-      if type(tree.right[1]) is BinTree:
+    if tree and tree.right:
+      if tree.right.key == None:
         self.as_dict(tree.right, True, path + [1])
       else:
-        self.lookup[tree.right[1]] = path + [1]
+        self.lookup[tree.right.key] = path + [1]
 
     return self.lookup
 
@@ -46,22 +70,21 @@ class Tree:
     if not recurse:
       self.bitlist = []
       tree = self.tree;
-    tree = tree[1];
 
     if tree.left:
       self.bitlist += BitArray.from_int(0, 1)
-      if type(tree.left[1]) is BinTree:
+      if tree.left.key == None:
         self.as_bitarray(tree.left, True)
       else:
         self.bitlist += BitArray.from_int(1, 1)
-        self.bitlist += BitArray.from_int(ord(tree.left[1]), 8)
+        self.bitlist += BitArray.from_int(ord(tree.left.key), 8)
 
     if tree.right:
-      if type(tree.right[1]) is BinTree:
+      if tree.right.key == None:
         self.as_bitarray(tree.right, True)
       else:
         self.bitlist += BitArray.from_int(1, 1)
-        self.bitlist += BitArray.from_int(ord(tree.right[1]), 8)
+        self.bitlist += BitArray.from_int(ord(tree.right.key), 8)
 
     return self.bitlist
 
@@ -82,6 +105,7 @@ class Tree:
     sortd = []
     while len(weights):
       key = max(weights, key=_sort)
-      sortd.append((weights.pop(key), key))
+      sortd.append(key)
+      weights.pop(key)
 
     return sortd
--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from classes.tree import Tree
 class Huffman:
   @staticmethod
   def compress(string):
+    if not string or not len(string):
+      throw
     tree = Tree(string)
     treedict = tree.as_dict()
     compressed = []
@@ -17,14 +19,31 @@ class Huffman:
 
   @staticmethod
   def uncompress(tree, compressed):
-    compressed = BitArray.from_bytes(compressed)
-    treeobj = Tree(None, tree)
+    string = BitArray.from_bytes(compressed)
+    tree = Tree(None, tree)
+
+    uncompressed = ''
+    sel = tree.tree
+    for bit in string:
+      if bit == 0:
+        sel = sel.left
+      else:
+        sel = sel.right
+      if (sel.key):
+        uncompressed += sel.key
+        sel = tree.tree
+
+    return uncompressed
 
 if __name__ == "__main__":
   # Compress string and get the tree and the compressed binary
-  (tree, compressed) = Huffman.compress('Hello')
+  try:
+    (tree, compressed) = Huffman.compress('Hey')
 
-  # Feed the tree and the string to the uncompress algorithm
-  uncompressed = Huffman.uncompress(tree, compressed)
+    # Feed the tree and the string to the uncompress algorithm
+    uncompressed = Huffman.uncompress(tree, compressed)
 
-  print(uncompressed)
+    print(uncompressed)
+  except:
+    print('Cannot compress 0 bytes')
+
